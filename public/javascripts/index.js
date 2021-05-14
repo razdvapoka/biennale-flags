f7044eb08ae0da3b22d9efa013cffb21981f1f4c
razdvapoka <razdvapoka@gmail.com>
2021-05-14 11:03:16 +0300

    :bug: Fix 100vh bug on mobile

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,13 @@
+const setCorrectVh = () => {
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  const vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+};
+
+// We listen to the resize event
+window.addEventListener("resize", setCorrectVh);
+
 const INTERVAL_MS = 1000;
 const SECOND_MS = 1000;
 const MINUTE_MS = SECOND_MS * 60;
